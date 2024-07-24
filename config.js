031e4841993b077f74020719450a7ab961a55f28
Zk Sinc Sr. <168566582+jediwonder23@users.noreply.github.com>
2024-07-24 17:26:31 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_VAJZF6gbY', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '7k01kbla1vicfjghh30ffallmd', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: '' // e.g. us-east-2
     },
     api: {
