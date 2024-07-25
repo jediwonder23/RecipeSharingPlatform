e57f546987931d1bb39c3e766ddc2ad407a8f5c1
Zk Sinc Sr. <168566582+jediwonder23@users.noreply.github.com>
2024-07-25 19:54:06 -0400

    Update config.js
    Updated the cognito pool

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_VAJZF6gbY', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '7k01kbla1vicfjghh30ffallmd', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_c3LAq9Ff3', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '18t148hoglm3ae762k2uu7p7i8', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://gmbe2anqbl.execute-api.us-east-1.amazonaws.com/dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
 
