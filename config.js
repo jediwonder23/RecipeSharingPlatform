407d8fcc1373ee0a1c1168be60c7c521c0d51af2
Zk Sinc Sr. <168566582+jediwonder23@users.noreply.github.com>
2024-07-24 23:10:53 -0400

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ window._config = {
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://aptzd1pmx9.execute-api.us-east-1.amazonaws.com/Dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://gmbe2anqbl.execute-api.us-east-1.amazonaws.com/dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
 
